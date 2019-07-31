b307ec25f07697224749467b0f4cd4b325fb8fc1
blank-black <497256303zby@gmail.com>
2019-07-31 22:43:31 +0100

    295

---
+++ golang/problems/295. Find Median from Data Stream.go
@@ -0,0 +1,62 @@
+package problems
+import "container/heap"
+
+// use two heap
+
+type IntHeap []int
+
+func (h IntHeap) Len() int { return len(h) }
+func (h IntHeap) Less(i, j int) bool { return h[i] < h[j] }
+func (h IntHeap) Swap(i, j int)      { h[i], h[j] = h[j], h[i] }
+
+func (h *IntHeap) Push(x interface{}) {
+	*h = append(*h, x.(int))
+}
+
+func (h *IntHeap) Pop() interface{} {
+	old := *h
+	n := len(old)
+	x := old[n-1]
+	*h = old[0 : n-1]
+	return x
+}
+
+type MedianFinder struct {
+	large, small *IntHeap
+}
+
+
+
+/** initialize your data structure here. */
+func Constructor() MedianFinder {
+	// h := &IntHeap{}
+	a := &IntHeap{}
+	b := &IntHeap{}
+	heap.Init(a)
+	heap.Init(b)
+	return MedianFinder{a, b}
+}
+
+
+func (this *MedianFinder) AddNum(num int)  {
+	if this.large.Len() == 0 || num > (*this.large)[0] {
+		heap.Push(this.large, num)
+	} else {
+		heap.Push(this.small, -num)
+	}
+	if this.large.Len() > this.small.Len() + 1 {
+		heap.Push(this.small, - heap.Pop(this.large).(int))
+	} else if this.small.Len() > this.large.Len() + 1 {
+		heap.Push(this.large, - heap.Pop(this.small).(int))
+	}
+}
+
+
+func (this *MedianFinder) FindMedian() float64 {
+	if this.large.Len() < this.small.Len() {
+		return float64(-(*this.small)[0])
+	} else if this.large.Len() > this.small.Len() {
+		return float64((*this.large)[0])
+	}
+	return float64(-(*this.small)[0] + (*this.large)[0]) / 2
+}
