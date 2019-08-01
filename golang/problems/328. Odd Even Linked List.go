b9077a7cb81d6d53c807904e292bab40bd46477f
blank-black <497256303zby@gmail.com>
2019-08-01 19:54:00 +0100

    322

---
+++ golang/problems/328. Odd Even Linked List.go
@@ -0,0 +1,31 @@
+package problems
+
+
+ // Definition for singly-linked list.
+  type ListNode struct {
+	  Val  int
+	  Next *ListNode
+  }
+func OddEvenList(head *ListNode) *ListNode {
+	if head == nil || head.Next == nil {
+		return head
+	}
+	odd := head
+	even := head.Next
+	he := head.Next
+	h := head.Next.Next
+	for h != nil {
+		odd.Next = h
+		odd = h
+		h = h.Next
+		if h == nil {
+			break
+		}
+		even.Next = h
+		even = h
+		h = h.Next
+	}
+	even.Next = nil
+	odd.Next = he
+	return head
+}
